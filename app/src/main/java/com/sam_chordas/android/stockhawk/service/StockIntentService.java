b8cf6e4cd4e75b07aee20501bff99ff2819d181f
Sureshkumar <sureshkumar@tenmiles.com>
2016-11-08 17:28:13 +0530

    fixed search stock crash issue

--- app/src/main/java/com/sam_chordas/android/stockhawk/service/StockIntentService.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/service/StockIntentService.java
@@ -4,6 +4,7 @@ import android.app.IntentService;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+
 import com.google.android.gms.gcm.TaskParams;
 import com.sam_chordas.android.stockhawk.utils.Constants;
 
@@ -12,23 +13,28 @@ import com.sam_chordas.android.stockhawk.utils.Constants;
  */
 public class StockIntentService extends IntentService {
 
-  public StockIntentService(){
-    super(StockIntentService.class.getName());
-  }
-
-  public StockIntentService(String name) {
-    super(name);
-  }
-
-  @Override protected void onHandleIntent(Intent intent) {
-    Log.d(StockIntentService.class.getSimpleName(), "Stock Intent Service");
-    StockTaskService stockTaskService = new StockTaskService(this);
-    Bundle args = new Bundle();
-    if (intent.getStringExtra(Constants.KEY_TAG).equals("add")){
-      args.putString("symbol", intent.getStringExtra("symbol"));
+    public StockIntentService() {
+        super(StockIntentService.class.getName());
     }
-    // We can call OnRunTask from the intent service to force it to run immediately instead of
-    // scheduling a task.
-    stockTaskService.onRunTask(new TaskParams(intent.getStringExtra(Constants.KEY_TAG), args));
-  }
+
+    public StockIntentService(String name) {
+        super(name);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        try {
+            Log.d(StockIntentService.class.getSimpleName(), "Stock Intent Service");
+            StockTaskService stockTaskService = new StockTaskService(this);
+            Bundle args = new Bundle();
+            if (intent.getStringExtra(Constants.KEY_TAG).equals("add")) {
+                args.putString("symbol", intent.getStringExtra("symbol"));
+            }
+            // We can call OnRunTask from the intent service to force it to run immediately instead of
+            // scheduling a task.
+            stockTaskService.onRunTask(new TaskParams(intent.getStringExtra(Constants.KEY_TAG), args));
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 }
